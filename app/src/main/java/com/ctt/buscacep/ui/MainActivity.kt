7c81e321475d11fda4a990e64c5437f03fd4b506
giuroperto <giu.roperto@gmail.com>
2021-04-10 02:11:55 -0300

    finished state spinner

--- app/src/main/java/com/ctt/buscacep/ui/MainActivity.kt
+++ app/src/main/java/com/ctt/buscacep/ui/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.ctt.buscacep.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.lifecycle.Observer
 import com.ctt.buscacep.R
 import com.ctt.buscacep.model.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cepField: EditText
     private lateinit var addressField: EditText
     private lateinit var cityField: EditText
-    private lateinit var stateField: EditText
+//    private lateinit var stateField: EditText
     private lateinit var formBtn: Button
     private lateinit var resultTxt: TextView
     private lateinit var stateSpn: Spinner
 
     private val CEPViewModel = CEPViewModel()
     private val addressViewModel = AddressViewModel()
+
+    private lateinit var selectedState: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         cepField = findViewById(R.id.edtCEP)
         addressField = findViewById(R.id.edtAddress)
         cityField = findViewById(R.id.edtCity)
-        stateField = findViewById(R.id.spnState)
+//        stateField = findViewById(R.id.spnState)
         formBtn = findViewById(R.id.btnBuscarCEP)
         resultTxt = findViewById(R.id.txtCEPResponse)
         stateSpn = findViewById(R.id.spnState)
@@ -38,45 +43,41 @@ class MainActivity : AppCompatActivity() {
             val spnAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, stateList)
             stateSpn.adapter = spnAdapter
 
-            stateSpn.onItemSelectedListener = object : AdapterView.onItemSelectedListener {
-                
+            stateSpn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    selectedState = stateList[position]
+//                    Toast.makeText(this@MainActivity, getString(R.string.selected_item) + " " + stateList[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
             }
 
         }
-
-//    spinner.onItemSelectedListener = object :
-//            AdapterView.OnItemSelectedListener {
-//        override fun onItemSelected(parent: AdapterView<*>,
-//                                    view: View, position: Int, id: Long) {
-//            Toast.makeText(this@MainActivity,
-//                    getString(R.string.selected_item) + " " +
-//                            "" + languages[position], Toast.LENGTH_SHORT).show()
-//        }
-//
-//        override fun onNothingSelected(parent: AdapterView<*>) {
-//            // write code to perform some action
-//        }
-//    }
-//}
-//}
-//}
 
         formBtn.setOnClickListener {
             val cep = cepField.text.toString()
             val address = addressField.text.toString()
             val city = cityField.text.toString()
-            val state = stateField.text.toString()
 
             if (cep.isNotEmpty()) {
                 searchCEP(cep)
             } else {
-                if (address.isNotEmpty() && city.isNotEmpty() && state.isNotEmpty()) {
-                    searchAddress(address, city, state)
+                if (address.isNotEmpty() && city.isNotEmpty() && selectedState.isNotEmpty()) {
+                    searchAddress(address, city, selectedState)
                 } else {
                     cepField.error = "Ou digite um CEP valido!"
                     addressField.error = "Ou digite um endereco valido..."
                     cityField.error = "E uma cidade valida..."
-                    stateField.error = "E um estado valido!"
+
+                    if (selectedState == resources.getStringArray(R.array.state_acronym)[0]) {
+                        val errorText: TextView? = stateSpn.selectedView as TextView?
+                        errorText?.error = ""
+                        errorText?.setTextColor(Color.RED)
+                        errorText?.text = "Selecione um estado!"
+                    }
                 }
             }
         }
@@ -117,3 +118,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+
+// TODO: 09/04/2021 testar breakpoint condicional 
+// TODO: 09/04/2021 testar breakpoint com alteracao de variavel 
